d8c79f7bb8e3c6bfb8c106fdf925f6563f9596a2
For Web <bestweb@europe.com>
2019-08-21 19:03:35 +0500

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function warmup(temperature) {
   // your implementation here
+  var warmup;
+
+  var temperature
+
+      temperature *=9;
+      temperature /=5;
+      temperature +=32;
+
+      warmup = temperature;
+
+  return warmup;
 };
